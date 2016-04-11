340c9bac28bd9181f2ff997669ac79d10d2fbb7b
Daniel Borlay <daniel.borlay@ericsson.com>
2016-04-11 18:25:12 +0200

    Restructure the directory tree
    I gave a better understandable structure for the sources.

--- build_docker.sh
+++ build_docker.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
 
-# Create Database
-mkdir -p database
-cp Dockerfile.database.service database/Dockerfile
-cp database.sh database/database.sh
-docker build -t bookstore_database database
+services=database webserver proxy reserve auth
 
-# Create Web server
-mkdir -p webserver
-cp Dockerfile.bookstore.service webserver/Dockerfile
-cp bookstore.sh webserver/bookstore.sh
-docker build -t bookstore webserver
+for service in services
+do
+    echo "Create ${service} for bookstore"
+    mkdir -p ${service}
+    cp Dockerfiles/Dockerfile.${service}.service ${service}/Dockerfile
+    cp scripts/${service}/* ${scripts}/
+    docker build -t bookstore_${service} ${service}
+done
 
-# Create Proxy
-mkdir -p proxy
-cp Dockerfile.proxy.service proxy/Dockerfile
-cp proxy.sh proxy/proxy.sh
-docker build -t bookstore_proxy proxy
-
-# Create Authserver
-mkdir -p auth
-cp Dockerfile.auth.service auth/Dockerfile
-cp auth.sh auth/auth.sh
-docker build -t bookstore_auth auth
-
-# Create Reserve service
-mkdir -p reserve
-cp Dockerfile.reserve.service reserve/Dockerfile
-cp reserve.sh reserve/reserve.sh
-docker build -t bookstore_reserve reserve
+echo "Microservices has been created!"
 
